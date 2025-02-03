f5700c29c8fc5349e7a93279235a404aa44463bb
Suresh Gundumogula <104331245+Suresh4723@users.noreply.github.com>
2025-02-03 23:56:31 +0530

    Update Alert dialog box.js

--- Alert dialog box.js
+++ Alert dialog box.js
@@ -2,4 +2,4 @@ alert("Hello, World!")
 window.alert("Hello, World! for the second time");
 alert(4 * 7);
 alert(true);
-alert("text 1", "text 2"); // only "text 1" will be dis
+alert("text 1", "text 2"); // only "text 1" will be 
